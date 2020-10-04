d6a2cc9f5bafd431a7ad88ef6d2b3e1e02878ae6
esther enriquez <cenriquez@esthers-MBP.fritz.box>
2020-10-04 18:43:31 +0200

    frist try

---
+++ holts.py
@@ -0,0 +1,12 @@
+# Getting user input
+temp = int(input("What's the temperature? "))
+
+
+# Conditionals
+if temp <= 0:
+    print("It’s freezing")
+elif temp >= 100:
+    print("It’s boiling")
+else:
+    print("It's alright")
+
